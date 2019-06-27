80def0d1fdf68b383be4d1fafa26abc9d341783e
biancaquintan <bsquintan@gmail.com>
2019-06-27 09:02:02 -0300

    add styles

--- App.js
+++ App.js
@@ -50,9 +50,9 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.titulo}>ToDo List</Text>
-        <FlatList data={this.state.itens} renderItem={this.renderItem} extraData={this.state}/>
-        <View>
-          <TextInput value={this.state.text} onChangeText={(text)=>{this.setState({text})}}/>
+        <FlatList style={styles.lista} data={this.state.itens} renderItem={this.renderItem} extraData={this.state}/>
+        <View style={styles.inputView}>
+          <TextInput value={this.state.text} style={styles.input} onChangeText={(text)=>{this.setState({text})}}/>
           <Button onPress={this.inserirItem} title="ADD"/>
         </View>
       </View>
@@ -65,6 +65,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#B3DECF',
+  },
+  cell: {
+    padding: 20,
+    backgroundColor: "#FFF",
+    margin: 10,
+    fontSize: 18,
+    borderRadius: 5
+  },
+  inputView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 20
+  },
+  input: {
+    backgroundColor: "#FFF",
+    borderColor: "#333",
+    borderWidth: 2,
+    padding: 10,
+    flex: 1,
+    borderRadius: 5,
+    marginRight: 20
   },
   titulo: {
     textAlign: 'center',
